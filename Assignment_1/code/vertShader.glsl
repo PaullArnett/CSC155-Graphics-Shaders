5116edc43b8ad5b716c92ab157a9dfe5cba6ef17
PaullArnett <paull.arnett@gmail.com>
2025-02-08 15:40:17 -0800

    just clean

--- Assignment_1/code/vertShader.glsl
+++ Assignment_1/code/vertShader.glsl
@@ -6,7 +6,6 @@ uniform vec4 colors[3];
 out vec4 fragColor;
 
 void main(void)
-<<<<<<< HEAD
 {
     if (offset.w == 0){
       if (gl_VertexID == 0)
@@ -47,9 +46,4 @@ void main(void)
       fragColor = colors[1];
     else 
       fragColor = colors[2];
-=======
-{ if (gl_VertexID == 0) gl_Position = vec4( 0.25+offset,-0.2, 0.0, 1.0);
-  else if (gl_VertexID == 1) gl_Position = vec4(-0.25+offset,-0.2, 0.0, 1.0);
-  else gl_Position = vec4( offset, 0, 0.0, 1.0);
->>>>>>> 22e68e69b76fb17709e5260020be1d23826586b1
 }
