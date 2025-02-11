94ebf07595e6dd0d48c8b4d6eef65fe1005e813f
Paul Arnett <paull.arnett@gmail.com>
2025-02-11 11:24:43 -0800

    Code cleaned and added comments

--- Assignment_1/code/Code.java
+++ Assignment_1/code/Code.java
@@ -1,9 +1,4 @@
-//for elapsed time see code in chpt 4
-//for moving in circle - send uniforms to shader (x,y)
-//for color changing, use uniform variable as well
-//For color gradient, each vertex is a solid color that becomes gradient in the middle
-//use out vec4 vertexColor and in vec4 vertexColor
-
+//
 package code;
 
 import javax.swing.*;
@@ -28,13 +23,14 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 	private int renderingProgram;
 	private int vao[] = new int[1];
 	private boolean circle = true;
-	private float x, y, radius;
+	private float x, y; 
+	private float radius = 0.3f;
 	private float z = 1;
 	private int w, colorCounter;
 	private float inc = 0.01f;
 	private double tf, elapsedTime, startTime;
 	private float[] colors = {
-		0.0f, 0.0f, 1.0f, 1.0f, 
+		0.0f, 0.0f, 1.0f, 1.0f,   //triangle starts blue
 		0.0f, 0.0f, 1.0f, 1.0f,
 		0.0f, 0.0f, 1.0f, 1.0f 
 	};
@@ -46,10 +42,7 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 		myCanvas.addGLEventListener(this);
 		this.add(myCanvas);
 		this.setVisible(true);
-
-		myCanvas.setFocusable(true);
 		myCanvas.addKeyListener(this);
-
 		Animator animator = new Animator(myCanvas);
 		animator.start();
 	}
@@ -57,12 +50,14 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 	@Override
 	public void keyPressed(java.awt.event.KeyEvent e) {
 		switch (e.getKeyCode()) {
+			//switches between circle and horizontal movement, and resets position
 			case java.awt.event.KeyEvent.VK_1:
 				circle = !circle;
 				x = 0;
 				y = 0;
 				startTime = System.currentTimeMillis();
 				break;
+			//cycles between different colors
 			case java.awt.event.KeyEvent.VK_2: 
 				if (colorCounter == 0) {
 					colors = new float[]{
@@ -87,14 +82,17 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 				}
 				colorCounter = (colorCounter + 1) % 3;
 				break;
+			//increase size
 			case java.awt.event.KeyEvent.VK_3: 
 				z = z + 0.25f;
 				break;
+			//decrease size
 			case java.awt.event.KeyEvent.VK_4: 
 				if(z >= 0.5f){
 				z = z - 0.25f;
 				}
 				break;
+			//changes orientation of triangle
 			case java.awt.event.KeyEvent.VK_5: 
 				w = (w+1) % 4;
 				break;
@@ -102,7 +100,7 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 	}
 	@Override
 	public void keyReleased(java.awt.event.KeyEvent e) {}
-
+	//empty functions, needed to be called for parent
 	@Override
 	public void keyTyped(java.awt.event.KeyEvent e) {}
 
@@ -113,9 +111,10 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 		gl.glClear(GL_DEPTH_BUFFER_BIT);
 		gl.glUseProgram(renderingProgram);
 
-		elapsedTime = System.currentTimeMillis() - startTime; // elapsedTime, startTime, and tf
- 		tf = elapsedTime / 1000.0;  // would all be declared of type double.
+		elapsedTime = System.currentTimeMillis() - startTime;
+ 		tf = elapsedTime / 1000.0; //gets time in terms of seconds
 
+		//circle & horizontal movements
 		if(circle){
 			x = 2 * radius * (float)Math.cos(tf);
 			y = 2 * radius * (float)Math.sin(tf);
@@ -126,13 +125,13 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 			if (x < -1.0f) inc = (float)tf; 
 			startTime = System.currentTimeMillis();
 		}
+
+		//sends uniform to vertShader of offset, scale, and orientation
 		int offsetLoc = gl.glGetUniformLocation(renderingProgram, "offset");
 		gl.glProgramUniform4f(renderingProgram, offsetLoc, x, y, z, w);
 
-		// Get location of 'colors' uniform in shader
+		//sends colors to shader
 		int colorsLoc = gl.glGetUniformLocation(renderingProgram, "colors");
-
-		// Send array of colors to shader
 		gl.glUniform4fv(colorsLoc, 3, colors, 0);
 
 		gl.glDrawArrays(GL_TRIANGLES,0,3);
@@ -144,10 +143,8 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 		gl.glGenVertexArrays(vao.length, vao, 0);
 		gl.glBindVertexArray(vao[0]);
 
-		radius = 0.3f;
+		//initializing start time
 		startTime = System.currentTimeMillis();
-
-
 
 		// Get and display OpenGL version
 		String openGLVersion = gl.glGetString(GL_VERSION);
@@ -159,6 +156,4 @@ public class Code extends JFrame implements GLEventListener, KeyListener
 	public static void main(String[] args) { new Code(); }
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
 	public void dispose(GLAutoDrawable drawable) {}
-
-
 }
